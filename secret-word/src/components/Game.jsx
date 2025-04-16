234fe593cdb956b3f69a591f8ab8bda318ee28da
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-04-16 12:10:37 -0300

    game screen

--- secret-word/src/components/Game.jsx
+++ secret-word/src/components/Game.jsx
@@ -3,9 +3,36 @@ import './Game.css'
 const Game = ( { verifyLetter } ) => {
 
     return (
-        <div>
-            <h1>Game</h1>
-            <button onClick={verifyLetter}> Finalizar Jogo </button>
+        <div className="game">
+            <p className='points'>
+
+                <span>Pontuação : 000</span>
+            </p>
+
+            <h3>Adivinhe a Palavra</h3>
+
+            <h3 className='tip'>
+                Dica sobre a palavra : <span>Dica...</span>
+            </h3>
+
+            <div className='wordContainer'>
+                <span className='letter'>A</span>
+                <span className='blankSquare'></span>
+            </div>
+
+            <div className="letterContainer">
+                <p>Tente enviar uma letra da palavra</p>
+                <form>
+                    <input type="text" name='letter' maxLength='1' required/>
+                    <button>Jogar</button>
+                </form>
+            </div>
+
+            <div className="wrongLettersContainer">
+                <p>Letras já utilizadas</p>
+                <span>A, </span>
+                <span>B, </span>
+            </div>
         </div>
     )
 
